f8dbb756f9b1dcf2f2a0c1e38c43c17ee566c23c
CandiceTomkins <87971865+CandiceTomkins@users.noreply.github.com>
2021-09-05 14:43:14 +0100

    Initial CSS

---
+++ src/app.js
@@ -0,0 +1,40 @@
+let now = new Date();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let date = now.getDate();
+let months = [
+  "January",
+  "Feburary",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "Spetember",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+let hours = now.getHours();
+
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let h2 = document.querySelector("h2");
+h2.innerHTML = `${day}, ${date}th ${month} ${hours}:${minutes}`;
