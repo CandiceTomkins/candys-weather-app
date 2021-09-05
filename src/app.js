7f5d359b729b4c1314457748450b914dfe439175
CandiceTomkins <87971865+CandiceTomkins@users.noreply.github.com>
2021-09-05 19:02:41 +0100

    Added search engine

--- src/app.js
+++ src/app.js
@@ -45,18 +45,18 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-function search(event) {
-  event.preventDefault();
-  let cityInputElement = document.querySelector("#city-input");
-  cityInputElement.innerHTML;
-  console.log(cityInputElement.value);
+function search(city) {
+  let apiKey = "a676f0f26e7447439669c990018a02b1";
+  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
 }
 
-let apiKey = "a676f0f26e7447439669c990018a02b1";
-let city = "Lisbon";
-let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("Lisbon");
 
 let form = document.querySelector("#search-form");
-form.addEventListener("submit", search);
+form.addEventListener("submit", handleSubmit);
